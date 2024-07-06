642c6d3a9e9e28e8b18d69dce500fb554e6c284c
eunjjang <83636742+eun-su-jeong@users.noreply.github.com>
2024-07-06 21:50:38 +0900

    feat: branch page

--- js/common.js
+++ js/common.js
@@ -1,4 +1,5 @@
 (()=>{
+    /* swiper */
     let swiper = new Swiper('.swiper', {
         direction: 'vertical',
 
@@ -19,5 +20,38 @@
             swiper.autoplay.start();
         }
     });
+
+    /* scroll */
+    let lastScrollTop = 0;
+    $(window).scroll(function() {
+        let currentScroll = $(this).scrollTop();
+        if (currentScroll > lastScrollTop) {
+            // 스크롤 다운
+            $('body').addClass('overscroll');
+            $('.ui-product .intro, .ui-product .toolbar').addClass('hidden').attr('aria-hidden', 'true');
+            $('.ui-product .extend').addClass('hidden').attr('aria-hidden', 'false');
+        } else {
+            // 스크롤 업
+            $('body').removeClass('overscroll');
+            $('.ui-product .intro, .ui-product .toolbar').removeClass('hidden').attr('aria-hidden', 'false');
+            $('.ui-product .extend').removeClass('hidden').attr('aria-hidden', 'true');
+        }
+        lastScrollTop = currentScroll;
+    });
+
+    /* card */
+    function onScroll() {
+        $('.card-cont').each(function() {
+            let elementTop = $(this).offset().top;
+            let viewportBottom = $(window).scrollTop() + $(window).height();
+
+            if (elementTop < viewportBottom) {
+                $(this).addClass('visible');
+            }
+        });
+    }
+
+    $(window).on('scroll', onScroll);
+    onScroll();
 
 })();
